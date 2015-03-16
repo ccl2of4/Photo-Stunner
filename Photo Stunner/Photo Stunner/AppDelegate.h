d7c4c23bef879d0016a815b80578120aa3e44719
ccl2of4 <ccl2of4@tx.rr.com>
2015-03-16 13:19:26 -0500

    added Xcode project files

---
+++ Photo Stunner/Photo Stunner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo Stunner
+//
+//  Created by Connor Lirot on 3/16/15.
+//  Copyright (c) 2015 Connor Lirot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
