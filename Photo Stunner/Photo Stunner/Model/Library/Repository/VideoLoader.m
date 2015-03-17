43d218ec5f935fa86f7d0339bffaa2e81a5979f2
ccl2of4 <ccl2of4@tx.rr.com>
2015-03-17 12:56:18 -0500

    removed 7.mp4

--- Photo Stunner/Photo Stunner/Model/Library/Repository/VideoLoader.m
+++ Photo Stunner/Photo Stunner/Model/Library/Repository/VideoLoader.m
@@ -38,7 +38,7 @@ NSString * const VideoLoaderModelChangedNotification = @"videoloader model chang
     static NSArray *videoNames = nil;
     if (!videoNames) {
         videoNames = @[
-            @"1.mp4",@"2.mp4",@"3.mp4",@"4.mp4",@"5.mp4",@"6.mp4",@"7.mp4",
+            @"1.mp4",@"2.mp4",@"3.mp4",@"4.mp4",@"5.mp4",@"6.mp4",
         ];
     }
     
